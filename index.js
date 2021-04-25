93ec7c6b8af8322c5e4e7d1f6f02f68f8608f43e
jmstman <79401578+jmstman@users.noreply.github.com>
2021-04-25 13:43:02 +0200

    Formatted index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express"),
+  morgan = require("morgan"),
+  bodyParser = require("body-parser"),
+  methodOverride = require("method-override");
+
+const app = express();
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+// Invoke Morgan middleware function
+app.use(morgan("common"));
+
+// Serving static files
+app.use(express.static("public"));
+
+// GET requests
+app.get("/", (req, res) => {
+  res.send("Welcome to the myFlix App!");
+});
+app.get("/documentation", (req, res) => {
+  res.sendFile("public/documentation.html", { root: __dirname });
+});
+
+app.use((err, req, res, next) => {
+  console.err(err.stack);
+  res.status(500).send("Something went wrong, try again later!");
+});
+
+//GET request to have a list of ALL movies in the Databas
+app.listen(8080, () => {
+  console.log("Your app is listening on port 8080.");
+});
