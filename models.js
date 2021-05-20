aa334b67e37c86147424cededabfd86dc78692f3
jmstman <79401578+jmstman@users.noreply.github.com>
2021-05-20 12:19:48 +0200

    Updated Schema

--- models.js
+++ models.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
-let movieSchema = mongoose.Schema({
+// Movies Mongoose Schema
+const movieSchema = mongoose.Schema({
   Title: { type: String, required: true },
   Description: { type: String, required: true },
   Genre: {
@@ -16,16 +17,36 @@ let movieSchema = mongoose.Schema({
   Featured: Boolean
 });
 
-let userSchema = mongoose.Schema({
+// Genres Mongoose Schema
+const genreSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Description: { type: String, required: true }
+});
+
+// Users Mongoose Schema
+const userSchema = mongoose.Schema({
   Username: { type: String, required: true },
   Password: { type: String, required: true },
   Email: { type: String, required: true },
   Birthday: Date,
-  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movie" }]
+  FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movies" }]
 });
 
-let Movie = mongoose.model("Movie", movieSchema);
-let User = mongoose.model("User", userSchema);
+// Directors Mongoose Schema
+const directorSchema = mongoose.Schema({
+  Name: { type: String, required: true },
+  Bio: String,
+  Birth: { type: Number, required: true },
+  Death: Number,
+  Movies: [{ type: mongoose.Schema.Types.ObjectId, ref: "Movies" }]
+});
+
+const Movie = mongoose.model("Movie", movieSchema);
+const User = mongoose.model("User", userSchema);
+const Director = mongoose.model("Director", directorSchema);
+const Genre = mongoose.model("Genre", genreSchema);
 
 module.exports.Movie = Movie;
 module.exports.User = User;
+module.exports.Genre = Genre;
+module.exports.Director = Director;
