6bf0ed6b98dbd825368c023ec3d83e9dd3cc811c
jmstman <79401578+jmstman@users.noreply.github.com>
2021-06-17 12:57:00 +0200

    allow app use cors

--- index.js
+++ index.js
@@ -27,21 +27,16 @@ const allowedOrigins = [ "http://localhost:1234", "http://paradiseflix.com"];
 
 //blocked out cors remove 'Access-Control-Allow-Origin'
 
-//app.use(
-  //cors({
-    //origin: (origin, callback) => {
-      //if (!origin) return callback(null, true);
-      //if (allowedOrigins.indexOf(origin) === -1) {
-        //If a specific origin isn’t found on the list of allowed origins
-        //let message =
-          //"The CORS policy for this application doesn’t allow access from origin " +
-          //origin;
-        //return callback(new Error(message), false);
-     // }
-      //return callback(null, true);
-    //}
-  //})
-//);
+app.use(cors({
+  origin: (origin, callback) => {
+    if(!origin) return callback(null, true);
+    if(allowedOrigins.indexOf(origin) === -1){ // If a specific origin isn’t found on the list of allowed origins
+      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message ), false);
+    }
+    return callback(null, true);
+  }
+}));
 
 //mongoose.connect
 mongoose.set("useFindAndModify", false);
