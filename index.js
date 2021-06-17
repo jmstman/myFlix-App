5025966852e26900f3d2b3234de72077f0580be2
jmstman <79401578+jmstman@users.noreply.github.com>
2021-06-17 14:01:00 +0200

    added space to identify cors app.use

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const passport = require("passport");
 const Models = require("./models.js");
+
 const cors = require("cors");
 app.use(cors());
 
